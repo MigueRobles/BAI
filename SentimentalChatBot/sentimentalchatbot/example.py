46cecad78a5b605471679fe099441be2223ff433
MigueRobles <miguerobles@correo.ugr.es>
2018-01-29 14:48:00 +0100

    Deleting

--- SentimentalChatBot/sentimentalchatbot/example.py
+++
@@ -1,8 +0,0 @@
-from pattern.en import sentiment
- 
-print sentiment(
-     "The movie attempts to be surreal by incorporating various time paradoxes,"
-     "but it's presented in such a ridiculous way it's seriously boring.")
-     
-print sentiment.positive("Happy", 0.1)
- 
